059f9bd9829abc89d04ac53f89bc46bd750a2d85
lalith-chennupati <56936664+lalith-chennupati@users.noreply.github.com>
2025-05-02 11:28:18 -0400

    Add generated test cases

---
+++ src/test/java/org/example/customerinfo/CustomerinfoApplicationTests.java
@@ -0,0 +1,15 @@
+```java
+package org.example.customerinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerinfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+```
